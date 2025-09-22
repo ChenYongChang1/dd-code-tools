8b19e432829f4b10c16c9d993aca0aedf2140caa
陈永昌 <chenyongchang@bwcj.com>
2025-09-22 10:39:09 +0800

    fix: pkg

--- packages/shared/vite.config.ts
+++ packages/shared/vite.config.ts
@@ -3,7 +3,6 @@ import { resolve } from "path";
 
 export default defineConfig({
   build: {
-    outDir: resolve(__dirname, "../../dist/shared"),
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
       name: "ddShared",
